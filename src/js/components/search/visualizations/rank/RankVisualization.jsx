39097bd9280fd3c0e4ae4931261b196d8f5a9b90
Nick Torres <nicholas.torres@kc.frb.org>
2023-10-03 15:17:14 -0600

    wip

--- src/js/components/search/visualizations/rank/RankVisualization.jsx
+++ src/js/components/search/visualizations/rank/RankVisualization.jsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { ResponsiveBar } from '@nivo/bar';
 import HorizontalChart from './chart/HorizontalChart';
 import RankVisualizationTooltip from './RankVisualizationTooltip';
 import ChartMessage from './RankVisualizationChartMessage';
@@ -70,17 +70,23 @@ export default class RankVisualization extends React.Component {
 
     render() {
         let chart = (<ChartMessage message="No data to display" />);
+        let chart2 = (<ChartMessage message="No data to display" />);
+
         let legend = null;
         if (this.props.loading) {
             chart = (<ChartMessage message="Loading data..." />);
+            chart2 = (<ChartMessage message="Loading data..." />);
         }
         else if (this.props.error) {
             chart = (<ChartMessage message="An error has occurred." />);
+            chart2 = (<ChartMessage message="An error has occurred." />);
             if (this.props.industryCodeError) {
                 chart = (<ChartMessage message="Industry codes are unavailable for Sub-Awards." />);
+                chart2 = (<ChartMessage message="Industry codes are unavailable for Sub-Awards." />);
             }
             else if (this.props.recipientError) {
                 chart = (<ChartMessage message="Paging to 10,000 records and above is not available for Spending by Recipient." />);
+                chart2 = (<ChartMessage message="Paging to 10,000 records and above is not available for Spending by Recipient." />);
             }
         }
         else if (this.props.dataSeries.length > 0) {
@@ -94,6 +100,125 @@ export default class RankVisualization extends React.Component {
                     height={height}
                     selectItem={this.selectItem}
                     deselectItem={this.deselectItem} />
+            );
+            chart2 = (
+                <ResponsiveBar
+                    data={data}
+                    keys={[
+                        'hot dog',
+                        'burger',
+                        'sandwich',
+                        'kebab',
+                        'fries',
+                        'donut'
+                    ]}
+                    indexBy="country"
+                    margin={{
+                        top: 50, right: 130, bottom: 50, left: 60
+                    }}
+                    padding={0.5}
+                    layout="horizontal"
+                    valueScale={{ type: 'linear' }}
+                    indexScale={{ type: 'band', round: true }}
+                    colors={{ scheme: 'greys' }}
+                    defs={[
+                        {
+                            id: 'dots',
+                            type: 'patternDots',
+                            background: 'inherit',
+                            color: '#38bcb2',
+                            size: 4,
+                            padding: 1,
+                            stagger: true
+                        },
+                        {
+                            id: 'lines',
+                            type: 'patternLines',
+                            background: 'inherit',
+                            color: '#eed312',
+                            rotation: -45,
+                            lineWidth: 6,
+                            spacing: 10
+                        }
+                    ]}
+                    fill={[
+                        {
+                            match: {
+                                id: 'fries'
+                            },
+                            id: 'dots'
+                        },
+                        {
+                            match: {
+                                id: 'sandwich'
+                            },
+                            id: 'lines'
+                        }
+                    ]}
+                    borderColor={{
+                        from: 'color',
+                        modifiers: [
+                            [
+                                'darker',
+                                1.6
+                            ]
+                        ]
+                    }}
+                    axisTop={null}
+                    axisRight={null}
+                    axisBottom={{
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: 0,
+                        legend: 'country',
+                        legendPosition: 'middle',
+                        legendOffset: 32
+                    }}
+                    axisLeft={{
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: 0,
+                        legend: 'food',
+                        legendPosition: 'middle',
+                        legendOffset: -40
+                    }}
+                    labelSkipWidth={12}
+                    labelSkipHeight={12}
+                    labelTextColor={{
+                        from: 'color',
+                        modifiers: [
+                            [
+                                'darker',
+                                1.6
+                            ]
+                        ]
+                    }}
+                    legends={[
+                        {
+                            dataFrom: 'keys',
+                            anchor: 'bottom-right',
+                            direction: 'column',
+                            justify: false,
+                            translateX: 120,
+                            translateY: 0,
+                            itemsSpacing: 2,
+                            itemWidth: 100,
+                            itemHeight: 20,
+                            itemDirection: 'left-to-right',
+                            itemOpacity: 0.85,
+                            symbolSize: 20,
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemOpacity: 1
+                                    }
+                                }
+                            ]
+                        }
+                    ]}
+                    role="application"
+                    ariaLabel="Nivo bar chart demo" />
             );
             legend = (
                 <div className="visualization-legend">
@@ -113,13 +238,22 @@ export default class RankVisualization extends React.Component {
         }
 
         return (
-            <section
-                className="results-visualization-rank-container"
-                aria-label="Spending by Category">
-                {chart}
-                {legend}
-                {tooltip}
-            </section>
+            <>
+                <section
+                    className="results-visualization-rank-container"
+                    aria-label="Spending by Category">
+                    {chart}
+                    {legend}
+                    {tooltip}
+                </section>
+                <section
+                    className="results-visualization-agencies-nivo"
+                    aria-label="Spending by Category">
+                    {chart2}
+                    {legend}
+                    {tooltip}
+                </section>
+            </>
         );
     }
 }
