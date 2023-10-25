78fd33d48b0887a14c638868f77fb273fe02c399
Nick Torres <nicholas.torres@kc.frb.org>
2023-10-25 13:20:12 -0600

    wip

--- src/js/components/search/visualizations/rank/RankVisualization.jsx
+++ src/js/components/search/visualizations/rank/RankVisualization.jsx
@@ -5,7 +5,8 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ResponsiveBar } from '@nivo/bar';
+// import { ResponsiveBar } from '@nivo/bar';
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import HorizontalChart from './chart/HorizontalChart';
 import RankVisualizationTooltip from './RankVisualizationTooltip';
 import ChartMessage from './RankVisualizationChartMessage';
@@ -70,23 +71,24 @@ export default class RankVisualization extends React.Component {
 
     render() {
         let chart = (<ChartMessage message="No data to display" />);
-        let chart2 = (<ChartMessage message="No data to display" />);
+        // let chart2 = (<ChartMessage message="No data to display" />);
+        let chart3 = (<ChartMessage message="No data to display" />);
 
         let legend = null;
         if (this.props.loading) {
             chart = (<ChartMessage message="Loading data..." />);
-            chart2 = (<ChartMessage message="Loading data..." />);
+            // chart2 = (<ChartMessage message="Loading data..." />);
         }
         else if (this.props.error) {
             chart = (<ChartMessage message="An error has occurred." />);
-            chart2 = (<ChartMessage message="An error has occurred." />);
+            // chart2 = (<ChartMessage message="An error has occurred." />);
             if (this.props.industryCodeError) {
                 chart = (<ChartMessage message="Industry codes are unavailable for Sub-Awards." />);
-                chart2 = (<ChartMessage message="Industry codes are unavailable for Sub-Awards." />);
+                // chart2 = (<ChartMessage message="Industry codes are unavailable for Sub-Awards." />);
             }
             else if (this.props.recipientError) {
                 chart = (<ChartMessage message="Paging to 10,000 records and above is not available for Spending by Recipient." />);
-                chart2 = (<ChartMessage message="Paging to 10,000 records and above is not available for Spending by Recipient." />);
+                // chart2 = (<ChartMessage message="Paging to 10,000 records and above is not available for Spending by Recipient." />);
             }
         }
         else if (this.props.dataSeries.length > 0) {
@@ -106,7 +108,7 @@ export default class RankVisualization extends React.Component {
             if (this.props.dataSeries.length === this.props.labelSeries.length) {
                 for (let i = 0; i < this.props.dataSeries.length; i++) {
                     dataStuff.push({
-                        data: this.props.dataSeries[i],
+                        value: this.props.dataSeries[i],
                         label: this.props.labelSeries[i],
                         desc: this.props.descriptions[i],
                         link: this.props.linkSeries[i]
@@ -114,46 +116,46 @@ export default class RankVisualization extends React.Component {
                 }
             }
 
-            chart2 = (
-                <ResponsiveBar
-                    data={dataStuff}
-                    indexBy={dataStuff.label}
-                    margin={{
-                        top: 50, right: 130, bottom: 50, left: 60
-                    }}
-                    padding={0.5}
-                    layout="horizontal"
-                    valueScale={{ type: 'linear' }}
-                    indexScale={{ type: 'band', round: true }}
-                    colors={{ scheme: 'greys' }}
-                    defs={[
-                        {
-                            id: 'dots',
-                            type: 'patternDots',
-                            background: 'inherit',
-                            color: '#38bcb2',
-                            size: 4,
-                            padding: 1,
-                            stagger: true
-                        },
-                        {
-                            id: 'lines',
-                            type: 'patternLines',
-                            background: 'inherit',
-                            color: '#eed312',
-                            rotation: -45,
-                            lineWidth: 6,
-                            spacing: 10
-                        }
-                    ]}
-                    axisTop={null}
-                    axisRight={null}
-                    axisBottom={null}
-                    axisLeft={null}
-                    legends={null}
-                    role="application"
-                    ariaLabel="Nivo bar chart demo" />
-            );
+            // chart2 = (
+            //     <ResponsiveBar
+            //         data={dataStuff}
+            //         indexBy={dataStuff.label}
+            //         margin={{
+            //             top: 50, right: 130, bottom: 50, left: 60
+            //         }}
+            //         padding={0.5}
+            //         layout="horizontal"
+            //         valueScale={{ type: 'linear' }}
+            //         indexScale={{ type: 'band', round: true }}
+            //         colors={{ scheme: 'greys' }}
+            //         defs={[
+            //             {
+            //                 id: 'dots',
+            //                 type: 'patternDots',
+            //                 background: 'inherit',
+            //                 color: '#38bcb2',
+            //                 size: 4,
+            //                 padding: 1,
+            //                 stagger: true
+            //             },
+            //             {
+            //                 id: 'lines',
+            //                 type: 'patternLines',
+            //                 background: 'inherit',
+            //                 color: '#eed312',
+            //                 rotation: -45,
+            //                 lineWidth: 6,
+            //                 spacing: 10
+            //             }
+            //         ]}
+            //         axisTop={null}
+            //         axisRight={null}
+            //         axisBottom={null}
+            //         axisLeft={null}
+            //         legends={null}
+            //         role="application"
+            //         ariaLabel="Nivo bar chart demo" />
+            // );
             legend = (
                 <div className="visualization-legend">
                     <div className="visualization-legend__circle" />
@@ -162,8 +164,68 @@ export default class RankVisualization extends React.Component {
                     </div>
                 </div>
             );
-        }
+            console.debug(dataStuff);
+            const CustomTick = (props) => {
+                const {
+                    x, y, stroke, payload, link
+                } = props;
+                console.debug("PAYLOAD: ", link[payload.index].link);
+                return (
+                    <g transform={`translate(${x},${y})`} width="40px">
+                        <a href={`${link[payload.index].link}`}>
+                            <text x={0} y={0} dy={0} textAnchor="end" fill="#666" fontSize={12} width="35px">
+                                {payload.value}
+                            </text>
+                        </a>
+                    </g>);
+            };
 
+            chart3 =
+            (
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        width="500px"
+                        height="600px"
+                        data={dataStuff}
+                        layout="vertical"
+                        barCategoryGap={20}
+                        margin={{
+                            top: 10,
+                            right: 10,
+                            left: 150,
+                            bottom: 10
+                        }}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis type="number" domain={[200000000, 160091459000]} />
+                        <YAxis type="category" dataKey="label" tick={<CustomTick link={dataStuff} />} fontSize="12px" link="link" />
+                        <Tooltip />
+                        <Bar dataKey="value" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                    </BarChart>
+                </ResponsiveContainer>
+            );
+        }
+        /* WORKING
+                <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                        width={600}
+                        height={500}
+                        data={dataStuff}
+                        layout="vertical"
+                        barCategoryGap={20}
+                        margin={{
+                            top: 10,
+                            right: 10,
+                            left: 50,
+                            bottom: 10
+                        }}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis type="number" domain={[200000000, 160091459000]} />
+                        <YAxis type="category" dataKey="label" fontSize="12px" />
+                        <Tooltip />
+                        <Bar dataKey="value" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                    </BarChart>
+                </ResponsiveContainer>
+*/
         let tooltip = null;
         if (this.state.showTooltip) {
             tooltip = (<RankVisualizationTooltip
@@ -174,22 +236,30 @@ export default class RankVisualization extends React.Component {
         return (
             <>
                 <div
-                    style={{ height: "200px" }}
+                    style={{ height: "900px", width: "800px" }}
                     className="results-visualization-agencies-nivo"
                     aria-label="Spending by Category NIVO">
-                    <section
-                        className="results-visualization-rank-container"
-                        aria-label="Spending by Category">
-                        {chart}
-                        {legend}
-                        {tooltip}
-                    </section>
-                    {chart2}
+                    {chart3}
                 </div>
             </>
         );
     }
 }
+/*
+<section
+                        className="results-visualization-rank-container"
+                        aria-label="Spending by Category">
+                        {chart3}
+                        {legend}
+                        {tooltip}
+                    </section>
+
+                    <section
+                        className="results-visualization-rank-container"
+                        aria-label="Spending by Category">
+                        {chart3}
+                    </section>
+*/
 
 RankVisualization.propTypes = propTypes;
 RankVisualization.defaultProps = defaultProps;
